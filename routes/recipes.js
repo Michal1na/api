6fb1731fb78ab73b1f70fb1e08cea977cf4e28b2
Michal1na <mbarnas2011@gmail.com>
2023-11-08 17:17:10 +0100

    Delete recipes and show hem by categories.

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const {db} = require('../public/db.json');
 
 
-/* GET users listing. */
+/* GET recipes listing. */
 recipeRouter
 
     .get('/get', function(req, res, next) {
@@ -29,7 +29,7 @@ recipeRouter
                 })
             }
         })
-
+        
         res.sendStatus(200)
     })
     .delete('/delete/:id', function(req, res, next) {
@@ -38,7 +38,7 @@ recipeRouter
                 console.log(error)
             } else {
                 let obj = JSON.parse(data);
-                let newobj = obj.filter((item) => item.id !== req.params.id);
+                let newobj = obj.filter((item) => item.id != req.params.id);
 
                 fs.writeFile("./public/db.json", JSON.stringify(newobj), err => {
                     if (err) {
@@ -47,7 +47,22 @@ recipeRouter
                 })
             }
         })
+       
         res.sendStatus(200)
+        
+    })
+    // show recipes by categories
+    .get('/:category', function (req, res, next) {
+        (fs.readFile("./public/db.json", "utf8", (error, data) => {
+            if (error) {
+                console.log(error);
+                return
+            } else {
+                let obj = JSON.parse(data);
+                res.json(obj.filter((item) => item.category === req.params.category));
+            }
+        }))
+        
     })
 module.exports = recipeRouter
 
